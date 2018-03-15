0d4e42b3325533823e8a3d79d11a333f2c6a855e
Yakhya Dabo <yakhyadabo@gmail.com>
2018-03-15 20:37:58 +0000

    [rating-service] New module

---
+++ rating-service/src/main/java/org/yakhya/spring/cloud/bookservice/RatingServiceApplication.java
@@ -0,0 +1,30 @@
+package org.yakhya.spring.cloud.bookservice;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@Api(value="rating", description="Rating service")
+@RequestMapping("/ratings")
+public class RatingServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(RatingServiceApplication.class, args);
+  }
+
+
+}
