acd1f045406976f20ded5c4f52d3f44a83c993e2
Yakhya Dabo <yakhyadabo@gmail.com>
2018-03-20 17:25:16 +0000

    [rating-service] Create web service

---
+++ rating-service/src/main/java/org/yakhya/spring/cloud/bookservice/Rating.java
@@ -0,0 +1,39 @@
+package org.yakhya.spring.cloud.bookservice;
+
+public class Rating {
+  private Long id;
+  private Long bookId;
+  private int stars;
+
+  public Rating(Long id, Long bookId, int stars) {
+    this.id = id;
+    this.bookId = bookId;
+    this.stars = stars;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getBookId() {
+    return bookId;
+  }
+
+  public void setBookId(Long bookId) {
+    this.bookId = bookId;
+  }
+
+  public int getStars() {
+    return stars;
+  }
+
+  public void setStars(int stars) {
+    this.stars = stars;
+  }
+
+  //standard getters and setters
+}
--- rating-service/src/main/java/org/yakhya/spring/cloud/bookservice/RatingServiceApplication.java
+++ rating-service/src/main/java/org/yakhya/spring/cloud/bookservice/RatingServiceApplication.java
@@ -11,10 +11,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -26,5 +29,21 @@ public class RatingServiceApplication {
     SpringApplication.run(RatingServiceApplication.class, args);
   }
 
+  private List<Rating> ratingList = Arrays.asList(
+      new Rating(1L, 1L, 2),
+      new Rating(2L, 1L, 3),
+      new Rating(3L, 2L, 4),
+      new Rating(4L, 2L, 5)
+  );
+
+  @GetMapping("")
+  public List<Rating> findRatingsByBookId(@RequestParam Long bookId) {
+    return bookId == null || bookId.equals(0L) ? Collections.EMPTY_LIST : ratingList.stream().filter(r -> r.getBookId().equals(bookId)).collect(Collectors.toList());
+  }
+
+  @GetMapping("/all")
+  public List<Rating> findAllRatings() {
+    return ratingList;
+  }
 
 }
